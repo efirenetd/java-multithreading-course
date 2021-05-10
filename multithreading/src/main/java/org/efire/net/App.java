976d23368fa6da13b0fb83a56b7ca48714dac12d
leqcar <leqcar@gmail.com>
2021-05-11 00:17:00 +0800

    second way, extend Thread class

--- multithreading/src/main/java/org/efire/net/App.java
+++ multithreading/src/main/java/org/efire/net/App.java
@@ -7,17 +7,20 @@ package org.efire.net;
 public class App 
 {
     public static void main( String[] args ) throws InterruptedException {
-        var newThread = new Thread(() -> {
-            //Throw unchecked exception
-            throw new RuntimeException("Error occurred");
-        });
-        newThread.setName("Misbehaving Thread");
-        newThread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            @Override
-            public void uncaughtException(Thread t, Throwable e) {
-                System.out.println("A critical error happens in the thread!!");
-            }
-        });
-        newThread.start();
+        var second_way_thread = new NewThread("Second Way Thread");
+        System.out.println("Before the start thread:" +Thread.currentThread().getName());
+        second_way_thread.start();
+        System.out.println("After the start thread:" +Thread.currentThread().getName());
+    }
+
+    static class NewThread extends Thread {
+        public NewThread(String name) {
+            super(name);
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Running new thread "+ this.currentThread().getName());
+        }
     }
 }
