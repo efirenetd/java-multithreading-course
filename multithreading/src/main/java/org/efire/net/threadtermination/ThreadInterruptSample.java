dff7713bcee4e0ff059a3c7af59f154aa00e07d5
leqcar <leqcar@gmail.com>
2021-05-11 14:08:47 +0800

    - compute large number to simulate long computation - added Thread.interrupted() if condition to interrupt the thread

--- multithreading/src/main/java/org/efire/net/threadtermination/ThreadInterruptSample.java
+++ multithreading/src/main/java/org/efire/net/threadtermination/ThreadInterruptSample.java
@@ -5,8 +5,10 @@ import java.math.BigInteger;
 public class ThreadInterruptSample {
 
     public static void main(String[] args) {
-        var thread = new Thread(new LongComputationTask(new BigInteger("2"), new BigInteger("10")));
+        var thread = new Thread(new LongComputationTask(
+                new BigInteger("20000000"), new BigInteger("1000000000")));
         thread.start();
+        //thread.interrupt();
     }
 
     static class LongComputationTask implements Runnable {
@@ -27,6 +29,10 @@ public class ThreadInterruptSample {
         private static BigInteger pow(BigInteger base, BigInteger power) {
             var result = BigInteger.ONE;
             for (BigInteger i = BigInteger.ZERO ; i.compareTo(power) !=0 ; i = i.add(BigInteger.ONE)) {
+                if (Thread.interrupted()) {
+                    System.out.println("Prematurely computed");
+                    return BigInteger.ZERO;
+                }
                 result = result.multiply(base);
             }
             return result;
