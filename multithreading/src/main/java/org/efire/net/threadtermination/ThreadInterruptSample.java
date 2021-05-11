12e6314c50c5bc0f4d1bc34ac5ffede607369d31
leqcar <leqcar@gmail.com>
2021-05-11 14:03:50 +0800

    added LongComputationTask implementation

--- multithreading/src/main/java/org/efire/net/threadtermination/ThreadInterruptSample.java
+++ multithreading/src/main/java/org/efire/net/threadtermination/ThreadInterruptSample.java
@@ -1,19 +1,43 @@
 package org.efire.net.threadtermination;
 
+import java.math.BigInteger;
+
 public class ThreadInterruptSample {
 
     public static void main(String[] args) {
-        var thread = new Thread(new BlockingTask());
+        var thread = new Thread(new LongComputationTask(new BigInteger("2"), new BigInteger("10")));
         thread.start();
-        thread.interrupt();
     }
 
+    static class LongComputationTask implements Runnable {
+
+        private BigInteger base;
+        private BigInteger power;
+
+        public LongComputationTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(base+"^"+power+" = "+pow(base, power));
+        }
+
+        private static BigInteger pow(BigInteger base, BigInteger power) {
+            var result = BigInteger.ONE;
+            for (BigInteger i = BigInteger.ZERO ; i.compareTo(power) !=0 ; i = i.add(BigInteger.ONE)) {
+                result = result.multiply(base);
+            }
+            return result;
+        }
+    }
     static class BlockingTask implements Runnable {
 
         @Override
         public void run() {
             try {
-                Thread.sleep(1000000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 System.out.println("Interrupt blocking task");
             }
