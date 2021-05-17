7f79784d9735fadb1904ed3d76656f663f0a5333
leqcar <leqcar@gmail.com>
2021-05-17 18:18:41 +0800

    Read Write lock - just using conventional ReentrantLock

---
+++ multithreading/src/main/java/org/efire/net/reentrantreadwritelock/InventoryDBApp.java
@@ -0,0 +1,120 @@
+package org.efire.net.reentrantreadwritelock;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.TreeMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class InventoryDBApp {
+
+    private static final int HIGHEST_PRICE = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryDatabase inventoryDatabase = new InventoryDatabase();
+
+        Random random = new Random();
+        for (int i = 0; i < 100_000; i++) {
+            inventoryDatabase.addItem(random.nextInt(HIGHEST_PRICE));
+        }
+
+        //One instance of Writer Thread
+        Thread writerThread = new Thread(() -> {
+           while(true) {
+               inventoryDatabase.addItem(random.nextInt(HIGHEST_PRICE));
+               inventoryDatabase.removeItem(random.nextInt(HIGHEST_PRICE));
+               try {
+                   Thread.sleep(10);
+               } catch (InterruptedException e) {
+               }
+           }
+        });
+       writerThread.setDaemon(true);
+       writerThread.start();
+
+       //Allocate 7 Threads for reading operations and create list to store
+        int numberOfReaderThread = 7;
+        List<Thread> readerThreads = new ArrayList<>();
+        for (int readerIndex = 0; readerIndex < numberOfReaderThread; readerIndex++) {
+            Thread reader = new Thread(() -> {
+                for (int i = 0; i < 100_000; i++) {
+                    int upperBoundPrice = random.nextInt(HIGHEST_PRICE);
+                    int lowerBoundPrice = upperBoundPrice > 0 ? random.nextInt(upperBoundPrice) : 0;
+                    inventoryDatabase.getNumberOfItemInPriceRange(lowerBoundPrice, upperBoundPrice);
+                }
+            });
+            reader.setDaemon(true);
+            readerThreads.add(reader);
+        }
+
+        long startReadingTime = System.currentTimeMillis();
+        for (Thread readerThread: readerThreads) {
+            readerThread.start();
+        }
+
+        for (Thread readerThread: readerThreads) {
+            readerThread.join();
+        }
+        long endReadingTime = System.currentTimeMillis();
+
+        System.out.println(String.format("Reading took %d ms", endReadingTime - startReadingTime));
+    }
+    public static class InventoryDatabase {
+        private TreeMap<Integer, Integer> priceToCount = new TreeMap<>();
+        private ReentrantLock lockObject = new ReentrantLock();
+
+        public int getNumberOfItemInPriceRange(int lowerbound, int upperbound) {
+            lockObject.lock();
+            try {
+                var fromKey = priceToCount.ceilingKey(lowerbound);
+                var toKey = priceToCount.floorKey(upperbound);
+
+                var rangeOfPrice = priceToCount.subMap(fromKey, true, toKey, true);
+
+                if (fromKey == 0 && toKey == 0) {
+                    return 0;
+                }
+
+                int sum = 0;
+                for (int numberOfPrice : rangeOfPrice.values()
+                ) {
+                    sum += numberOfPrice;
+                }
+
+                return sum;
+            }finally {
+                lockObject.unlock();
+            }
+        }
+
+        public void addItem(int price) {
+            lockObject.lock();
+            try {
+                var numberOfItemsForPrice = priceToCount.get(price);
+                if (numberOfItemsForPrice == null) {
+                    priceToCount.put(price, 1);
+                } else {
+                    priceToCount.put(price, numberOfItemsForPrice + 1);
+                }
+            } finally {
+                lockObject.unlock();
+            }
+        }
+
+        public void removeItem(int price) {
+            lockObject.lock();
+            try {
+                var numberOfItemsForPrice = priceToCount.get(price);
+                if (numberOfItemsForPrice == null || numberOfItemsForPrice == 1) {
+                    priceToCount.remove(price);
+                } else {
+                    priceToCount.put(price, numberOfItemsForPrice - 1);
+                }
+            }finally {
+                lockObject.unlock();
+            }
+        }
+    }
+
+
+}
